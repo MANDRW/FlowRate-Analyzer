2ce12bf92deb6f970da4ea0d8b03bf1e66d59a57
Mateusz Andrzejewski <mandrzejewski132@gmail.com>
2025-10-03 18:21:42 +0200

    small update with script

--- converter.py
+++ converter.py
@@ -1,5 +1,6 @@
 from heic2png import HEIC2PNG
 import os
+import shutil
 
 
 def converter(input_img, output_folder, id, type):
@@ -10,16 +11,24 @@ def converter(input_img, output_folder, id, type):
 
 if __name__ == "__main__":
     folder_type_path="close" # far or close
-    img_type_path="under" # under or good or over
+    img_type_path="good" # under or good or over
     input_folder=(f"{folder_type_path}/{img_type_path}")
     output_folder=(f"{folder_type_path}/{img_type_path}_png")
-    id = 1 # id of the first image (under)
-    type = 100 #100 - under, 010 - good, 001 - over
-    os.makedirs(output_folder, exist_ok=True)
+    id = 201 # id of the first image (1 - under, 201 - good, 401 - over)
+    type = "010" #100 - under, 010 - good, 001 - over
+    if(not os.path.exists(output_folder)):
+        os.makedirs(output_folder, exist_ok=True)
+    else:
+        shutil.rmtree(output_folder)
+        os.makedirs(output_folder, exist_ok=True)
+    if(not os.path.exists(input_folder)):
+        print("Folder doesn't exists")
+        exit()
     for img in os.listdir(input_folder):
-        if img.endswith(".HEIC") and img.startswith("IMG_"):
+        if (img.endswith(".HEIC") or img.endswith(".heic")) and img.startswith("IMG_"):
             input_img = os.path.join(input_folder, img)
             converter(input_img, output_folder,id,type)
+            print(id)
             id+=1
     print("DONE")
 
