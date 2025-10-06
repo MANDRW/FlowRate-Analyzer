abffa5640f011c8e3cd7d7a49f33ab89857339d9
Mateusz Andrzejewski <mandrzejewski132@gmail.com>
2025-10-06 21:08:16 +0200

    update with loading data

--- converter.py
+++ converter.py
@@ -10,12 +10,12 @@ def converter(input_img, output_folder, id, type):
     os.rename(converted_img_path, os.path.join(output_folder, f"{id}_{type}.png"))
 
 if __name__ == "__main__":
-    folder_type_path="far" # far or close
-    img_type_path="over" # under or good or over
+    folder_type_path="close" # far or close
+    img_type_path="good" # under or good or over
     input_folder=(f"{folder_type_path}/{img_type_path}")
     output_folder=(f"{folder_type_path}/{img_type_path}_png")
-    id = 401 # id of the first image (1 - under, 201 - good, 401 - over)
-    type = "001" #100 - under, 010 - good, 001 - over
+    id = 201 # id of the first image (1 - under, 201 - good, 401 - over)
+    type = "010" #100 - under, 010 - good, 001 - over
     if(not os.path.exists(output_folder)):
         os.makedirs(output_folder, exist_ok=True)
     else:
@@ -25,7 +25,7 @@ if __name__ == "__main__":
         print("Folder doesn't exists")
         exit()
     for img in os.listdir(input_folder):
-        if (img.endswith(".HEIC") or img.endswith(".heic")) and img.startswith("IMG_"):
+        if img.endswith(".HEIC") and img.startswith("IMG_"):
             input_img = os.path.join(input_folder, img)
             converter(input_img, output_folder,id,type)
             print(id)
--- preparing.py
+++ preparing.py
@@ -1,54 +1,79 @@
 import os
+from fileinput import filename
+
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-import numpy as np
-import random as rand
-import pandas as pd
-import matplotlib.pyplot as plt
 from tensorflow.keras.utils import load_img, to_categorical
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import random as rand
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-import tensorflow as tf
+from PIL import Image
 
-def samples(images,labels,quantity=9):
+
+def samples(images, labels, quantity=9):
     plt.figure(figsize=(10, 10))
     for i in range(quantity):
+        idx = rand.randint(0, len(images) - 1)
         plt.subplot(3, 3, i + 1)
-        num=rand.randint(0, len(images) - 1)
-        img = load_img(images[num])
-        plt.imshow(img)
-        plt.title(num)
+        plt.imshow(images[idx])
+        plt.title(f"Klasa: {labels[idx]}")
         plt.axis("off")
     plt.show()
 
 
+
 class DataPrep:
-    def __init__(self,path,img_size=(512,512),batch_size=32,test_size=0.2,random_state=42):
+    def __init__(self,path,img_size=(512,512),batch_size=32,test_size=0.2,random_state=42,crop_ratio=0.5):
         self.path = path
         self.img_size = img_size
         self.batch_size = batch_size
         self.test_size = test_size
         self.random_state = random_state
+        self.crop_ratio=crop_ratio
         self.label_map = {"100":0,"010":1,"001":2} # 100 - under, 010 - good, 001 - over
 
-    def load(self):
+    def crop(self,image,crop_ratio=0.5):
+        h, w, _ = image.shape
+        new_h, new_w = int(h * crop_ratio), int(w * crop_ratio)
+        left = max(0, (w - new_w) // 2)
+        top = max(0, (h - new_h) // 2)
+        right = left + new_w
+        bottom = top + new_h
+        return image[top:bottom, left:right]
+
+    def load(self, crop=False):
         images = []
         labels = []
         for img in os.listdir(self.path):
-            if (img.endswith(".png") or img.endswith(".PNG")):
-                path = os.path.join(self.path, img)
-                img=load_img(path,target_size=self.img_size)
-                img=np.array(img)/255.0
-                label = [int(x) for x in img.split("_")[1].split(".")[0]]
-                label=self.label_map(label)
-                images.append(img)
-                labels.append(label)
+            try:
+                if (img.endswith(".png") or img.endswith(".PNG")):
+                    path = os.path.join(self.path, img)
+                    print(img)
+                    file_label = img.split("_")[1].split(".")[0]
+                    if(crop==True):
+                        image=load_img(path)
+                        image=np.array(image)
+                        image=self.crop(image,self.crop_ratio)
+                        image=Image.fromarray(image).resize(self.img_size)
+                    else:
+                        image = load_img(path, target_size=self.img_size)
+                    image=np.array(image)/255.0
+                    label = self.label_map[file_label]
+                    images.append(image)
+                    labels.append(label)
+            except Exception as e:
+                print(f"Error loading image {img}: {e}")
+                continue
         images = np.array(images)
-        labels = to_categorical(labels,num_classes=3)
+        labels = np.array(labels)
         return images, labels
 
     def prep(self,augment=True):
         images, labels = self.load()
         x_train, x_val, y_train, y_val = train_test_split(images, labels, test_size=self.test_size, random_state=self.random_state, stratify=labels)
+        y_train = to_categorical(y_train, num_classes=3)
+        y_val = to_categorical(y_val, num_classes=3)
         if(augment==True):
             train_datagen = ImageDataGenerator(
                 rotation_range=20,
@@ -67,3 +92,18 @@ class DataPrep:
         train_gen = train_datagen.flow(x_train, y_train, batch_size=self.batch_size)
         val_gen= val_datagen.flow(x_val, y_val, batch_size=self.batch_size)
         return train_gen, val_gen
+
+
+if __name__ == "__main__":
+    path = "far_flowrate_dataset"
+    img_size = (4032//6, 3024//6)  # Resize images to 672x504
+    batch_size = 16
+    test_size = 0.2
+    random_state = 42
+    crop_ratio=0.5
+
+    data_prep = DataPrep(path, img_size, batch_size, test_size, random_state,crop_ratio)
+    train_gen, val_gen = data_prep.prep(augment=True)
+
+    images, labels = data_prep.load(True)
+    samples(images, labels, quantity=9)
